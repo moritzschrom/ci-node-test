057ce18a20a79405d2aff06c617fc3cade185f17
Moritz Schrom <moritz.schrom@schrom.co>
2021-05-14 13:05:52 +0200

    Added super duper test.

--- src/sum/sum.test.js
+++ src/sum/sum.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 0 + 0 to equal 0', () => {
     expect(sum(0, 0)).toBe(0);
 });
+
+test('adds 100 + 0 to equal 100', () => {
+    expect(sum(100, 0)).toBe(0);
+});
